d3261dd88afccddb0040840d45c178eee60adcb5
AMIR HASHMI <amirhashmi017@gmail.com>
2024-08-17 14:35:59 +0500

    Conditional Logic

---
+++ JavaScript/JavaScript(Conditions).js
@@ -0,0 +1,18 @@
+console.log("Hello I am conditional tutorial")
+let age = 0;
+if (age >= 18) {
+    console.log("You can Drive.");
+}
+else if (age == 0) {
+    console.log("Are you mad??");
+}
+else {
+    console.log("You can't drive.");
+}
+//Type Checking Operator
+if ("3" !== 3) {
+    console.log("True");
+}
+else { console.log("False"); }
+//Ternary Operator
+(5 + 6 == 11) ? console.log("True") : console.log("False");
