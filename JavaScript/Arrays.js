9e2e37f0fa26de747dc8d527c9d110349ee501de
AMIR HASHMI <amirhashmi017@gmail.com>
2024-08-18 14:58:02 +0500

    Update JS Array

--- JavaScript/Arrays.js
+++ JavaScript/Arrays.js
@@ -1,31 +1,31 @@
-// let arr=[1,2,4,5,7];
-// let arr2=[3,7,6];
-// let arr3=["asad",7,"ashir"];
-// console.log(arr);
-// console.log(arr.length);
-// console.log(arr[2]);
-// //arrays are mutable while strings are immutable means originally can't be changed.
-// arr[2]=2;
-// console.log(arr);
-// console.log(arr.toString());
-// console.log(arr.join("   and   "));
-// arr.pop();//Removes element from end
-// console.log(arr);
-// arr.push("Hashmi");//Insert element at end elements can be of different types.
-// console.log(arr);
-// arr.shift();//Removes element from beginning.
-// console.log(arr);
-// arr.unshift("Amir");//Insert element at beginning.
-// console.log(arr);
-// delete arr[4];//Delete the value of element but space still allocates.
-// console.log(arr);
-// console.log(arr.concat(arr2,arr3));
-// arr2.sort();
-// console.log(arr2);
-// arr.splice(3,5);
-// console.log(arr);
-// arr.splice(0,1,555,777);
-// console.log(arr);
-// console.log(arr.slice(1,3));
-// arr.reverse();
-// console.log(arr);
+let arr=[1,2,4,5,7];
+let arr2=[3,7,6];
+let arr3=["asad",7,"ashir"];
+console.log(arr);
+console.log(arr.length);
+console.log(arr[2]);
+//arrays are mutable while strings are immutable means originally can't be changed.
+arr[2]=2;
+console.log(arr);
+console.log(arr.toString());
+console.log(arr.join("   and   "));
+arr.pop();//Removes element from end
+console.log(arr);
+arr.push("Hashmi");//Insert element at end elements can be of different types.
+console.log(arr);
+arr.shift();//Removes element from beginning.
+console.log(arr);
+arr.unshift("Amir");//Insert element at beginning.
+console.log(arr);
+delete arr[4];//Delete the value of element but space still allocates.
+console.log(arr);
+console.log(arr.concat(arr2,arr3));
+arr2.sort();
+console.log(arr2);
+arr.splice(3,5);
+console.log(arr);
+arr.splice(0,1,555,777);
+console.log(arr);
+console.log(arr.slice(1,3));
+arr.reverse();
+console.log(arr);
