53ebcc8b31994dd6f2b5273256db17b3f7d2e5dc
AMIR HASHMI <amirhashmi017@gmail.com>
2024-08-19 18:39:07 +0500

    JS DOM(Child,Parent,Siblings)

--- JavaScript/DOM(Child,Parent).js
+++ JavaScript/DOM(Child,Parent).js
@@ -2,6 +2,9 @@ console.log("Hello World");
 document.body.childNodes[1];
 let cont=document.body.childNodes[1]
 cont.firstChild
-cont.firstElementChild.style.
-cont.lastChild
+cont.lastElementChild.style.backgroundColor="green";
 cont.childNodes[1].style.color="red";
+cont.children[0].style.backgroundColor="black";
+cont.children[2].style.border="5px solid brown";
+cont.children[2].previousElementSibling.style.backgroundColor="red";
+document.body.childNodes[1].children[2].nextElementSibling.style.backgroundColor="brown";
