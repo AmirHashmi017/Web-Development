aec8d851d34a8a27a3030c87c96771a804761c8c
AMIR HASHMI <amirhashmi017@gmail.com>
2024-08-19 14:41:52 +0500

    DOM(Child/Parent)

---
+++ JavaScript/DOM(Child,Parent).js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+document.body.childNodes[1];
+let cont=document.body.childNodes[1]
+cont.firstChild
+cont.firstElementChild.style.
+cont.lastChild
+cont.childNodes[1].style.color="red";
