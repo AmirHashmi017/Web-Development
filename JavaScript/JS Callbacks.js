3978da6c195ec9aa0c40550593b7f13bbb924727
AMIR HASHMI <amirhashmi017@gmail.com>
2024-08-21 20:05:41 +0500

    JS CallBacks

--- JavaScript/JS Callbacks.js
+++ JavaScript/JS Callbacks.js
@@ -15,3 +15,18 @@ const loadscript=(src,callback) => {
   document.head.append(sc);
 }
 loadscript("../Bootstrap/bootstrap-5.3.3-dist/js/bootstrap.min.js", callback);
+const changecolor=(element,color)=>
+{
+    element.style.backgroundColor=color;
+}
+const changetext=(changecolor)=>
+{
+    element=document.querySelector(".box");
+    element.innerHTML="Amir Hashmi";
+    changecolor(element,"red");
+}
+document.querySelector(".box").addEventListener("click",()=>
+{
+    changetext(changecolor);
+})
+
