5adf4464641928f5c5ccafaf615ec0215b5eba1e
AMIR HASHMI <amirhashmi017@gmail.com>
2024-08-21 21:17:41 +0500

    JS Promises

--- JavaScript/JS Promises.js
+++ JavaScript/JS Promises.js
@@ -1,4 +1,4 @@
-console.log("This is Promises");
+// console.log("This is Promises");
 let prom1=new Promise((resolve,reject)=>
 {
     let a=Math.random();
@@ -11,7 +11,7 @@ let prom1=new Promise((resolve,reject)=>
     {
         console.log("Yes I am Done")
         resolve("amir");
-    },3000)
+    },100)
 }
 })
 let prom2=new Promise((resolve,reject)=>
@@ -26,31 +26,53 @@ let prom2=new Promise((resolve,reject)=>
         {
             console.log("Yes I am Done2")
             resolve("amir2");
-        },2000)
+        },100)
     }
     })
 
-    // prom1.then((a)=>
-    // {
-    //     console.log(a);
-    // }).catch((err)=>
-    // {
-    //     console.log(err);
-    // })
-    // let p3=Promise.all([prom1,prom2])
-    // p3.then((a)=>
-    // {
-    //     console.log(a);
-    // }).catch((e)=>
-    // {
-    //     console.log(e);
-    // })
-    // 
-    let p3=Promise.race([prom1,prom2])
+    prom1.then((a)=>
+    {
+        console.log(a);
+    }).catch((err)=>
+    {
+        console.log(err);
+    })
+    let p3=Promise.all([prom1,prom2])
     p3.then((a)=>
     {
         console.log(a);
     }).catch((e)=>
     {
         console.log(e);
+    })
+    
+    let p8=Promise.race([prom2,prom1])
+    p8.then((a)=>
+    {
+        console.log("passed");
+    }).catch((e)=>
+    {
+        console.log("Failed");
+    })
+    //Then will execute if all given promises are resolved otherwise catch will be executed.
+    let p4=Promise.all([prom1,prom2])
+    p4.then((m)=>{
+        console.log("Successed");}
+    ).catch((err)=>{
+    console.log("Failed");})
+    let p6=Promise.allSettled([prom1,prom2])
+    p6.then((a)=>
+    {
+        console.log(a);
+    }).catch((err)=>
+    {
+        console.log(err);
+    })
+    let p7=Promise.any([prom1,prom2])
+    p7.then((a)=>
+    {
+        console.log("Success");
+    }).catch((err)=>
+    {
+        console.log("Fail");
     })
