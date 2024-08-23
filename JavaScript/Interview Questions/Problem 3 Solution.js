2f2ee8d30988f9d9984bdb03008926ef02cabdc5
AMIR HASHMI <amirhashmi017@gmail.com>
2024-08-23 10:42:23 +0500

    JS Problem 3 Solution

--- JavaScript/Interview Questions/Problem 3 Solution.js
+++ JavaScript/Interview Questions/Problem 3 Solution.js
@@ -1,1 +1,7 @@
 // The Mirror Mirror: Imagine you have a string, and you need to create a new string that is a mirror image of the original. Write a function that appends the reversed version of the original string to itself.
+function ReverseString(inputstr)
+{
+let reverse=inputstr.split('').reverse().join('');
+return inputstr+reverse;
+}
+console.log(ReverseString("AmirHashmi"));
