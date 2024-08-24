b11b243941384b10582af009f72fe96931b6f424
AMIR HASHMI <amirhashmi017@gmail.com>
2024-08-24 19:06:42 +0500

    Problem 11 Solution

--- JavaScript/Interview Questions/Problem 11 Solution.js
+++ JavaScript/Interview Questions/Problem 11 Solution.js
@@ -1,13 +1,17 @@
 // The Array Filterer: You are building a search feature for your e-commerce site. Write a function named filterProducts that takes an array of product objects and a filter criterion. The function should return a new array containing only the products that match the filter criterion.
 function SearchFilters(products,filter)
 {
-    let filtered=[]
-for (const product of products) {
-    if(product.toLowerCase().includes(filter.toLowerCase()))
+    let filtered=products.filter((p)=>
     {
-filtered.push(product);
-    }
-}
+return p.toLowerCase().includes(filter.toLowerCase());
+    })
+    //Second Method
+// for (const product of products) {
+//     if(product.toLowerCase().includes(filter.toLowerCase()))
+//     {
+// filtered.push(product);
+//     }
+// }
 return filtered;
 }
 let products=["Pasta","Zinger Burger","Pizza","Chicken pakistan Broast","Lemonade","pAkora"];
