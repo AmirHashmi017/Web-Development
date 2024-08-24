d6433089dae83a53b671a8c27ee7a9a64f4cdd8d
AMIR HASHMI <amirhashmi017@gmail.com>
2024-08-24 20:16:15 +0500

    Problem 14 Solution

--- CSS/UltraEdit Website.js
+++ CSS/UltraEdit Website.js
@@ -1,3 +1,4 @@
+// The Window Scroller: You are developing a single-page application with a smooth scrolling effect. Implement a function named smoothScrollToTop that smoothly scrolls the window to the top when called.
 function smoothScrollToTop() {
     window.scrollTo({
         top: 0,
