a0bc9bd13f0c1f595036d889605fcbb949f45d2b
AMIR HASHMI <amirhashmi017@gmail.com>
2024-08-27 18:21:01 +0500

    Added Play buttons and time functionalities

--- Spotify Clone/js/script.js
+++ Spotify Clone/js/script.js
@@ -1,6 +1,7 @@
+let currentsong=new Audio();
 async function getSongs()
 {
-let a=await fetch ("http://127.0.0.1:3000/Spotify%20Clone/songs/");
+let a=await fetch ("http://127.0.0.1:3002/Spotify%20Clone/songs/");
 let response= await a.text();
 let div=document.createElement("div");
 div.innerHTML=response;
@@ -14,15 +15,40 @@ songs.push(as[element].href.split(`/songs/`)[1]);
 }
 return songs;
 }
-const playMusic=(track)=>
+function secondsToMinutesSeconds(seconds) {
+    if (isNaN(seconds) || seconds < 0) {
+        return "00:00";
+    }
+
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
+currentsong.addEventListener("timeupdate",()=>
 {
-    let musicsrc = `http://127.0.0.1:3000/Spotify%20Clone/songs/${track}`;
-    var audio = new Audio(musicsrc);
-    audio.play();
+    console.log(currentsong.currentTime,currentsong.duration);
+    document.querySelector(".songtime").innerHTML=`${secondsToMinutesSeconds(currentsong.currentTime)}/${secondsToMinutesSeconds(currentsong.duration)}`;
+})
+const playMusic=(track,pause = true)=>
+{
+    if(!pause)
+    {
+        document.querySelector("#play").src="img/pause.svg";
+    }
+    let musicsrc = `http://127.0.0.1:3002/Spotify%20Clone/songs/${track}`;
+    currentsong.src=musicsrc;
+    currentsong.play();
+    document.querySelector(".songinfo").innerHTML=decodeURI(track);
+    document.querySelector(".songtime").innerHTML=`${secondsToMinutesSeconds(currentsong.currentTime)}/${secondsToMinutesSeconds(currentsong.duration)}`;
 }
 async function main()
 {
 let songs=await getSongs();
+playMusic(songs[0]);
 let songul=document.querySelector(".songList").getElementsByTagName("ul")[0];
 for (const song of songs) {
     songul.innerHTML=songul.innerHTML+`
@@ -47,6 +73,18 @@ Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEa
         playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
     })
 })
-
+document.querySelector("#play").addEventListener("click",()=>
+{
+    if(currentsong.paused)
+    {
+        currentsong.play();
+        document.querySelector("#play").src="img/pause.svg";
+    }
+    else
+    {
+        currentsong.pause();
+        document.querySelector("#play").src="img/play.svg";
+    }
+})
 }
 main();
