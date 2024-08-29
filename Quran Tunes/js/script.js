dfdb9d2b4626b3aab7d287dc20fd10133f951449
AMIR HASHMI <amirhashmi017@gmail.com>
2024-08-29 14:01:28 +0500

    Automating albums through APIs

--- Quran Tunes/js/script.js
+++ Quran Tunes/js/script.js
@@ -1,7 +1,9 @@
 let currentsong = new Audio();
 let songs;
-async function getSongs() {
-    let a = await fetch("http://127.0.0.1:3000/Quran%20Tunes/songs/");
+let currfolder;
+async function getSongs(folder) {
+    currfolder=folder;
+    let a = await fetch(`http://127.0.0.1:3000/Quran%20Tunes/${currfolder}/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -9,9 +11,33 @@ async function getSongs() {
     let songs = [];
     for (let element = 0; element < as.length; element++) {
         if (as[element].href.endsWith(".mp3")) {
-            songs.push(as[element].href.split(`/songs/`)[1]);
+            songs.push(as[element].href.split(`/${currfolder}/`)[1]);
         }
     }
+    let songul = document.querySelector(".songList").getElementsByTagName("ul")[0];
+    songul.innerHTML="";
+    for (const song of songs) {
+        songul.innerHTML = songul.innerHTML + `
+    <li>
+    <img class="invert" src="img/music.svg" alt="play">
+    <div class="info">
+    <div> ${song.replaceAll("%20", " ")}</div>
+    <div>Al Quran</div>
+    </div>
+    <div class="playnow">
+        <span>Play Now</span>
+        <img class="invert" src="img/play.svg" alt="Play">
+    </div>
+    
+</li>
+    `
+    }
+    // playMusic(songs[0]);
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach((e) => {
+        e.addEventListener("click", () => {
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+        })
+    })
     return songs;
 }
 function secondsToMinutesSeconds(seconds) {
@@ -41,38 +67,15 @@ const playMusic = (track, pause = false) => {
     if (!pause) {
         document.querySelector("#play").src = "img/pause.svg";
     }
-    let musicsrc = `http://127.0.0.1:3000/Quran%20Tunes/songs/${track}`;
+    let musicsrc = `http://127.0.0.1:3000/Quran%20Tunes/${currfolder}/${track}`;
     currentsong.src = musicsrc;
     currentsong.play();
     document.querySelector(".songinfo").innerHTML = decodeURI(track);
     document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentsong.currentTime)} / ${secondsToMinutesSeconds(currentsong.duration)}`;
 }
 async function main() {
-    songs = await getSongs();
-
-    let songul = document.querySelector(".songList").getElementsByTagName("ul")[0];
-    for (const song of songs) {
-        songul.innerHTML = songul.innerHTML + `
-    <li>
-    <img class="invert" src="img/music.svg" alt="play">
-    <div class="info">
-    <div> ${song.replaceAll("%20", " ")}</div>
-    <div>Al Quran</div>
-    </div>
-    <div class="playnow">
-        <span>Play Now</span>
-        <img class="invert" src="img/play.svg" alt="Play">
-    </div>
-    
-</li>
-    `
-    }
-    // playMusic(songs[0]);
-    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach((e) => {
-        e.addEventListener("click", () => {
-            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
-        })
-    })
+    songs = await getSongs("songs");
+    displayAlbums();
     document.querySelector("#play").addEventListener("click", () => {
         if (currentsong.paused) {
             currentsong.play();
@@ -81,6 +84,21 @@ async function main() {
         else {
             currentsong.pause();
             document.querySelector("#play").src = "img/play.svg";
+        }
+    })
+}
+async function displayAlbums()
+{
+    let a = await fetch("http://127.0.0.1:3000/Quran%20Tunes/songs/");
+    let response = await a.text();
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let anchors=div.getElementsByTagName("a");
+    Array.from(anchors).forEach((e)=>
+    {
+        if(e.href.includes("/songs"))
+        {
+            console.log(e.href.split("/").slice(-1)[0]);
         }
     })
 }
@@ -107,4 +125,14 @@ document.querySelector("#next").addEventListener("click", e => {
 document.querySelector(".range").getElementsByTagName("input")[0].addEventListener("change", e => {
     currentsong.volume = parseInt(e.target.value) / 100;
 })
+Array.from(document.getElementsByClassName("card")).forEach(e=>
+{
+    e.addEventListener("click",async items=>
+        {
+            songs=await getSongs(`songs/${items.currentTarget.dataset.folder}`);
+            playMusic(songs[0]);
+        }
+        )
+}
+)
 main();
